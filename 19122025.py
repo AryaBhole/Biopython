52d0f067fc44fbbfb662906ae54df6deba0c975e
AryaBhole <bholeaarya@gmai.com>
2025-12-21 17:29:13 +0530

    Modified Motif file

--- 19122025.py
+++ 19122025.py
@@ -1,3 +1,5 @@
+#Motif Checker
+
 # x = input("Enter a nucleotide sequence : ")
 
 # print(f"Number of A in sequence : {x.count("A")}")
@@ -7,14 +9,29 @@
 
 # print(f"The total length of the input sequence is : {len(x)}")
 
-s = "PALYDEHRDSETLY"
-m = "HRD"
+# s = "PALYDEHRDSETLY"
+# m = "HRD"
 
-count = 0
-for y in range(len(s)):
-    if m[0] == s[y] and m[1] == s[y + 1] and m[2] == s[y + 2] and y + 1 < len(s) :
-        count += 1
+# count = 0
+# for y in range(len(s)):
+#     if m[0] == s[y] and m[1] == s[y + 1] and m[2] == s[y + 2] and y + 1 < len(s) :
+#         count += 1
 
-if count > 0:
-    print("Motif is present in the sequence")
+# if count > 0:
+#     print("Motif is present in the sequence")
 
+seq = input("Enter a sequence : ").upper()
+mot = input("Enter the motif : ")
+
+f = False
+a = 0
+
+for x in range(len(seq)):
+    part = seq[a : a + len(mot)]
+    if part == mot:
+        f = True
+        print(f"Motif is present in the sequence at {a+1} position")
+    a += 1
+
+if f != True:
+    print("Motif is not present in the sequence")
