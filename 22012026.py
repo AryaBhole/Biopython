972360fe0471e31b552a8c964745ad96abb88473
AryaBhole <bholeaarya@gmail.com>
2026-01-23 11:23:54 +0530

    list manipulation

---
+++ 22012026.py
@@ -0,0 +1,6 @@
+mylist = ["a", "b", "c", "a", "a", "b"]
+
+for a in mylist :
+    mylist.remove('a')
+
+print(mylist)
---
+++ 23012026.py
@@ -0,0 +1,8 @@
+mylist = [x*x for x in range(1, 21) if x%2 == 0]
+
+print(mylist)
+
+fruits = ["Apple", "Banana"]
+mylist = [i.upper() for i in fruits]
+
+print(mylist)
