f3461633e92c16556527a57147e2e4467216774f
AryaBhole <bholeaarya@gmail.com>
2026-01-23 12:06:14 +0530

    list aliasing

--- 23012026.py
+++ 23012026.py
@@ -3,6 +3,20 @@ mylist = [x*x for x in range(1, 21) if x%2 == 0]
 print(mylist)
 
 fruits = ["Apple", "Banana"]
-mylist = [i.upper() for i in fruits]
+fruits = [i.upper() for i in fruits]
 
-print(mylist)
+print(fruits)
+
+#aliasing of lists
+
+x = [10, 20, 30]
+y = x
+print(x, id(x))
+print(y, id(y))
+x[2] = 99
+y[2] = 10
+print(x, id(x))
+print(y, id(y))
+
+#use y = x[:] to get diffrent storage in lists
+
