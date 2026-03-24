2660af99374a9101f05f8748173eeae80f1467d1
AryaBhole <bholeaarya@gmail.com>
2026-03-24 18:58:52 +0530

    just the general init file with all which i used ai for and renamed some stuff

--- Minor2/genome_pipelines/__init__.py
+++ Minor2/genome_pipelines/__init__.py
@@ -4,7 +4,7 @@
 from .sequence_fetcher import fetch_sequence_from_ncbi, save_fasta
 from .sequence_analyzer import analyze_sequence, print_summary_table
 from .orf_motif_finder import find_orfs, find_motifs, print_orf_table, codon_usage
-from .visualizer import generate_plots
+from .visualizer import generate_all_plots
 
 __all__ = [
     "fetch_sequence_from_ncbi",
@@ -15,5 +15,5 @@ __all__ = [
     "find_motifs",
     "print_orf_table",
     "codon_usage",
-    "generate_plots"
+    "generate_all_plots"
 ]
