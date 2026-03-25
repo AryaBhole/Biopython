074f1cdba4adbd1cbf48ca0108aac590316619c4
AryaBhole <bholeaarya@gmail.com>
2026-03-25 09:12:33 +0530

    removed __all__ from init as it is not required

--- Minor2/genome_pipelines/__init__.py
+++ Minor2/genome_pipelines/__init__.py
@@ -5,15 +5,3 @@ from .sequence_fetcher import fetch_sequence_from_ncbi, save_fasta
 from .sequence_analyzer import analyze_sequence, print_summary_table
 from .orf_motif_finder import find_orfs, find_motifs, print_orf_table, codon_usage
 from .visualizer import generate_all_plots
-
-__all__ = [
-    "fetch_sequence_from_ncbi",
-    "save_fasta",
-    "analyze_sequence",
-    "print_summary_table",
-    "find_orfs",
-    "find_motifs",
-    "print_orf_table",
-    "codon_usage",
-    "generate_all_plots"
-]
